e33157ef4f9788c40eda18a0be275c66694062ae
Amanuel Haile <amanhailebeyene@gmail.com>
2025-09-27 11:15:17 +0300

    add files

---
+++ index.js
@@ -0,0 +1,43 @@
+let day = 4;
+
+switch(day){
+    case 1:
+        console.log("it is monday");
+        break;
+        case 2:
+        console.log("it is Thusday");
+        break;
+
+          case 3:
+        console.log("it is WENSDAY");
+        break;
+
+          case 4:
+        console.log("it is TERSDAY");
+        break;
+
+          case 5:
+        console.log("it is FRIDAY");
+        break;
+          case 6:
+        console.log("it is SATERDAY");
+        break;
+          case 7:
+        console.log("it is SUNDAY");
+        break;
+       
+
+}
+
+/*let testScore = 28;
+let letterGrade;
+
+switch(true){
+  case testScore >= 30 :
+  letterGrade = "A";
+  break;
+   case testScore >=20 :
+  letterGrade = "B";
+
+}
+console.log(letterGrade);*/
